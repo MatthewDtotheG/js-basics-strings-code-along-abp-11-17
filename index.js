6a40df53d9d2f7f59001d27fb868512543ebcad1
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-07 03:52:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Write your code in this file!
 let currentUser = 'Matt';
 let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-let currentUser = 'MATT';
 let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!';
